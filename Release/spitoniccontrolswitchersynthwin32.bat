a6c59f3e40f5c467d504c9e3dab0d5ca491d6000
Stephane Poirier <stephane.poirier@oifii.org>
2016-03-02 20:08:02 -0500

    First commit

---
+++ Release/spitoniccontrolswitchersynthwin32.bat
@@ -0,0 +1,2 @@
+start spitoniccontrolswitchersynthwin32.exe "E-MU ASIO" 6 7  "E-MU ASIO" 2 3 1000.0 -1 1 1 900 1000 220 1 0 0 24 255 200 0 "mywindowclass" "mywindowtitle" "" ""
+rem start spitoniccontrolswitchersynthwin32.exe "E-MU ASIO" 6 7  "E-MU ASIO" 2 3 100.0 -1 1 1 900 1000 220 1 0 0 24 255 200 0 "mywindowclass" "mywindowtitle" "" ""
